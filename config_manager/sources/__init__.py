0c5fbb6b1e4cecf6ed007028fff0ac4d3b8b83c5
sirhCC <ccharles199220@gmail.com>
2025-07-27 16:00:34 -0400

    Add TomlSource to the configuration sources in ConfigManager

--- config_manager/sources/__init__.py
+++ config_manager/sources/__init__.py
@@ -6,5 +6,6 @@ from .base import BaseSource
 from .environment import EnvironmentSource
 from .json_source import JsonSource
 from .yaml_source import YamlSource
+from .toml_source import TomlSource
 
-__all__ = ["BaseSource", "EnvironmentSource", "JsonSource", "YamlSource"]
+__all__ = ["BaseSource", "EnvironmentSource", "JsonSource", "YamlSource", "TomlSource"]
