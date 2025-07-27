ce6fd65e70fc63963bed9a99d3d7358a401c06d3
sirhCC <ccharles199220@gmail.com>
2025-07-27 17:08:26 -0400

    Add ProfileManager and related functions to module exports

--- config_manager/__init__.py
+++ config_manager/__init__.py
@@ -3,5 +3,6 @@ A modern and extensible configuration management library for Python.
 """
 
 from .config_manager import ConfigManager
+from .profiles import ProfileManager, ConfigProfile, create_profile_source_path, profile_source_exists
 
-__all__ = ["ConfigManager"]
+__all__ = ["ConfigManager", "ProfileManager", "ConfigProfile", "create_profile_source_path", "profile_source_exists"]
