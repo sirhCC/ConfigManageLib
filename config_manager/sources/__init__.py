1d254320b40c112c64be8a005b4f2599b9c2037b
sirhCC <ccharles199220@gmail.com>
2025-07-27 15:22:26 -0400

    Add YamlSource to configuration sources in ConfigManager

--- config_manager/sources/__init__.py
+++ config_manager/sources/__init__.py
@@ -5,5 +5,6 @@ Configuration sources for the ConfigManager.
 from .base import BaseSource
 from .environment import EnvironmentSource
 from .json_source import JsonSource
+from .yaml_source import YamlSource
 
-__all__ = ["BaseSource", "EnvironmentSource", "JsonSource"]
+__all__ = ["BaseSource", "EnvironmentSource", "JsonSource", "YamlSource"]
